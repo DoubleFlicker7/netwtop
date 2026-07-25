68ead674c1f5e706d818777cc6e47fb0f55f30e3
DoubleFlicker7 <eddyxu2000@163.com>
2026-07-25 14:58:02 +0800

    Prepare 0.1.1 PPA revision for Jammy

---
+++ script.sh
@@ -0,0 +1,5 @@
+DEBFULLNAME='DoubleFlicker7' \
+DEBEMAIL='eddyxu2000@gmail.com' \
+dch --newversion "0.1.1-1~ppa1~ubuntu22.04.1" \
+    --distribution "jammy" \
+    'Publish netwtop through the Launchpad PPA.'
